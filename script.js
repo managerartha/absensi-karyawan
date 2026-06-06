3f7ff729c08aa961534bca269e1898600651461f
managerartha <manag3r.artha@gmail.com>
2026-06-07 00:41:30 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,63 @@
-function login(){
+const API_URL =
+"https://script.google.com/macros/s/AKfycbyVqjC_BsVD73Q8Yh_DPTvXbbm_qeipIAm4loyk9rE4VMw5e9yVC3Q7hM1bJdxZadmo/exec";
 
-  alert(
-    "Frontend Github Pages berhasil"
-  );
+async function login(){
+
+  const username =
+  document.getElementById(
+    "username"
+  ).value;
+
+  const password =
+  document.getElementById(
+    "password"
+  ).value;
+
+  try{
+
+    const response =
+    await fetch(
+      API_URL,
+      {
+        method:"POST",
+        headers:{
+          "Content-Type":
+          "application/json"
+        },
+        body:JSON.stringify({
+          action:"login",
+          username:username,
+          password:password
+        })
+      }
+    );
+
+    const result =
+    await response.json();
+
+    if(result.status){
+
+      alert(
+        "Login Berhasil\n\n" +
+        result.nama
+      );
+
+    }else{
+
+      alert(
+        "Username atau Password salah"
+      );
+
+    }
+
+  }catch(error){
+
+    console.log(error);
+
+    alert(
+      "Gagal terhubung ke server"
+    );
+
+  }
 
 }
